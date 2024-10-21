7ad618a1f4262abe8075177194aad1982c0beba4
saivamshi <saivamshik11@gmail.com>
2024-10-21 20:59:19 +0530

    added files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module todoapplicatation
+
+go 1.23.2
+
+require golang.org/x/oauth2 v0.23.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	
+)
+func main(){
+	fmt.Println("###### welcome to our todo list application #######")
+
+
+	fmt.Println("List of my todos")
+	
+}
